ac206b6b23568c13c353977bfcd3cf309df48844
Felipe Pino <felipepinoredes@gmail.com>
2019-09-10 15:27:32 -0400

    Lista de todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { todos } from './todos.json';
+
+import TodoForm from './components/TodoForms';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index) {
+    if (window.confirm('Are you sure you want to delete it?')) {
+      this.setState({
+        todos: this.state.todos.filter((e,i) => {
+          return i !== index
+        })
+      })
+    }
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}
+              >
+                Delete
+              </button>
+            </div>
+        </div>
+        </div>
+      )
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="App" >
+      <nav className="navbar navbar-dark bg-dark">
+        <a href="" className="text-white">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              { this.state.todos.length }
+            </span>
         </a>
-      </header>
+      </nav>
+
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-md-3">
+          <TodoForm onAddTodo={this.handleAddTodo}/>
+         
+          </div>
+          <div className="col-md-9">
+            <div className="row">
+            { todos }
+            </div>
+          </div>
+        </div>
+      </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        
     </div>
   );
 }
-
+}
 export default App;
